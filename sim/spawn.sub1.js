0dc3d0f4f788c482db2de4f2f2038187c08ce8fd
ARC190 <ARC190@web.de>
2018-07-10 22:13:47 +0200

    fine-tune 07/10/18

--- sim/spawn.sub1.js
+++ sim/spawn.sub1.js
@@ -2,17 +2,15 @@ var SpawnSub = {
     
     run:function(ScreepCounter){
     
-    /*for(var i=1;i<=(ScreepCounter[0][0]+ScreepCounter[0][1]+ScreepCounter[0][2]);i++){
+   /* for(var i=1;i<=(ScreepCounter[0][0]+ScreepCounter[0][1]+ScreepCounter[0][2]);i++){
         for(var name in Memory.creeps){
-            if(name != ('Creep' + i)){
+            if(name != ('Creep' + i))
                 var newName = ('Creep'+i);
-            }
         }
-    }*/
-    var newName='creep'+Game.time;
-    
+    }
+    */
+    var newName= 'Creep'+Game.time;
     if(ScreepCounter[1][2].length < 1 && ScreepCounter[1][0].length >= 2 && ScreepCounter[1][1].length >= 1) {
-        
         Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE], newName, 
             {memory: {role: 'builder'}});        
     }
@@ -21,7 +19,7 @@ var SpawnSub = {
         Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE], newName, 
             {memory: {role: 'upgrader'}});        
     }
-        
+    
      if(ScreepCounter[1][0].length < 4) {
         Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE], newName, 
             {memory: {role: 'harvester'}});        
